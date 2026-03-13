3801f7cfc26ae699f6c68106fc23e42fe5e4ee93
CesarBecval <cesarbecerravalencia@gmail.com>
2026-03-13 09:32:08 -0600

    feat: Primer commit de la Práctica 2.

---
+++ Practica_2.hs
@@ -0,0 +1,22 @@
+
+{-
+    Práctica 2 - Recursión Estructural, Listas, Árboles y
+                 Definiciones Inductivas
+    Profesor: Dr. Marco Vladimir Lemus Yañez
+    Ayudante: Dr. Fernando Cruz Pineda
+    Fecha: 27/02/2026 
+
+    Integrantes del equipo:
+    - Becerra Valencia César
+    - Cortes Nava José Luis
+    - Díaz Anavia Javier Omar
+-}
+
+-- 4.1 Análisis Sintáctico - Función contarConectivos
+
+-- 4.2 Tabla de Verdad - Función tabla
+
+-- 4.3 Propiedades Semanticas - Función esTautologia
+
+-- 4.4 Lenguaje Proposicional Mínimo - Definición de nuevo tipo algebraico y función aMin
+
